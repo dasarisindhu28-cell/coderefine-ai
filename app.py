99e45b344f91ecef84e59e2de7dfb364a1f06d26
dasarisindhu28-cell <dasarisanjeeva62@gmail.com>
2026-02-21 13:56:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -278,5 +278,6 @@ with gr.Blocks(title="CodeRefine AI") as demo:
 # =========================
 # LAUNCH
 # =========================
-demo.launch(server_name="0.0.0.0", server_port=10000)
+demo.launch(server_name="0.0.0.0", server_port=port)
+
 
